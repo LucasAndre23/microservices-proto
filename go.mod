84ad28a8d5f24e59e00d95286ddd392967731cbd
Lucasandre23 <lucasandre152015@gmail.com>
2025-12-18 20:29:07 -0300

    part2

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/lucasandre23/microservices-proto/golang/payment
 
-go 1.22.2
+go 1.25.1
 
 require github.com/lucasandre23/microservices-proto/golang/order v0.0.0
 
--- run.sh
+++ run.sh
@@ -5,32 +5,32 @@ GITHUB_EMAIL=lucasandre152015@gmail.com
 SERVICE_NAME=payment
 RELEASE_VERSION=v1.2.3
 
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest 
+echo "Instalando/Atualizando plugins gRPC..."
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 export PATH="$PATH:$(go env GOPATH)/bin"
-source ~/.zshrc
 
-echo "Generating Go source code"
-mkdir -p golang
-protoc --go_out=./golang \
+
+echo "Gerando código fonte Go para: ${SERVICE_NAME}"
+
+mkdir -p golang/${SERVICE_NAME}
+
+protoc --proto_path=. \
+  --go_out=./golang/${SERVICE_NAME} \
   --go_opt=paths=source_relative \
-  --go-grpc_out=./golang \
+  --go-grpc_out=./golang/${SERVICE_NAME} \
   --go-grpc_opt=paths=source_relative \
- ./${SERVICE_NAME}/*.proto
+  ./${SERVICE_NAME}.proto
 
-echo "Generated Go source code files"
+echo "Arquivos gerados:"
 ls -al ./golang/${SERVICE_NAME}
 
+echo "Configurando Go Module..."
 cd golang/${SERVICE_NAME}
-go mod init \
-  github.com/${GITHUB_USERNAME}/microservices-proto/golang/${SERVICE_NAME} || true
-go mod tidy || true
-#cd ../../
-#git config --global user.email ${GITHUB_EMAIL}
-#git config --global user.name ${GITHUB_USERNAME}
-#git add . && git commit -am "proto update" || true
-#git push -u origin HEAD
-#git tag -d ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
-#git push --delete origin ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
-#git tag -fa ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION} \
-#  -m "ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-#git push origin refs/tags/ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+
+rm -f go.mod 
+go mod init github.com/${GITHUB_USERNAME}/microservices-proto/golang/${SERVICE_NAME}
+go mod tidy
+
+echo "Script finalizado com sucesso."
